011c63519bc212f889356d6466fc567828c231b5
itsolution-git <roy.smith0820@gmail.com>
2018-03-18 18:02:32 +0200

    web animation added

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,16 +45,14 @@ import 'core-js/es6/set';
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-import 'hammerjs';
-import 'web-animations-js';
+// import 'hammerjs';
 
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
