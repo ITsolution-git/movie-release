d8f9a5587bbd74a3638019daaf14ee1f4d9dc1c1
itsolution-git <roy.smith0820@gmail.com>
2018-03-19 10:35:46 -0700

    Created AdminService, which checks for User role in Admin Area

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 // Material Modules
 import {
   MatSidenavModule,
@@ -12,18 +11,15 @@ import {
   MatTableModule,
   MatProgressSpinnerModule
 } from '@angular/material';
-
 // Routing Module
 import { AdminRoutingModule } from './admin-routing.module';
-
 // Guard Services
 import { AdminRoleGuardService } from '../core/auth/admin-role-guard.service';
-
 // Services
+import { AdminService } from './admin.service';
 import { UploadService } from './_services/upload/upload.service';
 import { UsersService } from './users/users.service';
 import { ArticlesService } from './articles/articles.service';
-
 // Components
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -38,7 +34,7 @@ import { SelectMovieDialogComponent } from './articles/select-movie-dialog/selec
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,    
+    ReactiveFormsModule,
     AdminRoutingModule,
     MatSidenavModule,
     MatIconModule,
@@ -66,7 +62,8 @@ import { SelectMovieDialogComponent } from './articles/select-movie-dialog/selec
     AdminRoleGuardService,
     UploadService,
     UsersService,
-    ArticlesService
+    ArticlesService,
+    AdminService
   ]
 })
 export class AdminModule { }
