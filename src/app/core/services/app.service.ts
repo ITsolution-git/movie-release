5f7702fac08a0fe80895bd02feb680482bc737f8
itsolution-git <roy.smith0820@gmail.com>
2018-03-24 06:53:46 -0700

    Created GoToMovieDetails and getMovieUrlById functions

--- src/app/core/services/app.service.ts
+++ src/app/core/services/app.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material';
+// AngularFire
+import { AngularFireDatabase } from 'angularfire2/database';
+// Constants
+import { DB_COL } from '../../constants';
 // Components
 import { AuthDialogComponent } from '../../content/shared/auth-dialog/auth-dialog.component';
 
@@ -10,7 +14,9 @@ export class AppService {
 
   constructor(
     private http: HttpClient,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private afDb: AngularFireDatabase
   ) { }
 
   urlOptimizeText(text: string): string {
@@ -41,4 +47,24 @@ export class AppService {
       });
   }
 
+  goToMovieDetails(movieId: string) {
+    // Get the Movie URL by MovieID from firebase movie_results collection
+    this.getMovieUrlById(movieId)
+      .then(res => {
+        console.log(res['url']);
+        this.router.navigate([res['url']]);
+      });
+  }
+
+  getMovieUrlById(movieId: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.afDb.list(`${DB_COL.MOVIES_RESULTS}`, ref => ref.orderByChild('id').equalTo(movieId)).valueChanges()
+        .subscribe(res => {
+          console.log(res);
+          console.log(res[0]);
+          console.log(res[0]['url']);
+          resolve(res[0]);
+        });
+    })
+  }
 }
