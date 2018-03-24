01fcc725f4d5ac9c7a0872dcb9aef24b44c37868
itsolution-git <roy.smith0820@gmail.com>
2018-03-24 06:10:01 -0700

    Moved Duplicate Title Checker to Settings Component

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from '../../core/services/firebase/firebase.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,76 +7,7 @@ import { FirebaseService } from '../../core/services/firebase/firebase.service';
 })
 export class DashboardComponent implements OnInit {
 
-  duplicateTitles: any[] = [];
-  duplicateMovieDetails: any;
-  loading: boolean;
-  constructor(
-    private fS: FirebaseService
-  ) {
-    this.getAllMoviesResults();
-  }
+  constructor() { }
 
   ngOnInit(): void { }
-
-  getAllMoviesResults() {
-    this.duplicateTitles = [];
-    this.loading = true;
-    this.fS.getAllMoviesResults()
-      .then(res => {
-        this.checkDuplicateMovieTitles('slug', res)
-          .then(duplicates => {
-            for (let i = 0; i < duplicates.length; i++) {
-              // console.log(duplicates[i]);
-              const element = duplicates[i];
-              this.getDuplicateMovieDetailsBySlug(element)
-                .then(movie => {
-                  // console.log(movie);
-                  this.duplicateTitles.push(movie);
-                  // console.log(this.duplicateTitles);
-                  this.loading = false;
-                });
-            }
-          });
-      });
-  }
-
-  checkDuplicateMovieTitles(propertyName, collection): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      const duplicateTitles = [];
-      const moviesObject = {};
-      collection.map((item) => {
-        const moviePropertyName = item[propertyName];
-        if (moviePropertyName in moviesObject) {
-          duplicateTitles.push(moviePropertyName);
-        } else {
-          moviesObject[moviePropertyName] = item;
-          delete item.duplicate;
-        }
-      });
-      resolve(duplicateTitles);
-    });
-  }
-
-  getDuplicateMovieDetailsByTitle(movieTitle: string): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      this.fS.getMoviesByTitle(movieTitle)
-        .then(res => {
-          resolve(res);
-          // this.duplicateMovieDetails = res;
-          // console.log(this.duplicateMovieDetails);
-        });
-    });
-  }
-
-  getDuplicateMovieDetailsBySlug(movieSlug: string): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      this.fS.getMoviesBySlug(movieSlug)
-        .then(res => {
-          resolve(res);
-          // this.duplicateMovieDetails = res;
-          // console.log(this.duplicateMovieDetails);
-        });
-    });
-  }
-
 }
--- src/app/admin/settings/settings.component.ts
+++ src/app/admin/settings/settings.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+// Services
 import { SitemapService } from '../../core/services/sitemap/sitemap.service';
 import { ApiService } from '../../core/services/api/api.service';
+import { FirebaseService } from '../../core/services/firebase/firebase.service';
 
 @Component({
   selector: 'app-settings',
@@ -11,14 +12,20 @@ import { ApiService } from '../../core/services/api/api.service';
 })
 export class SettingsComponent implements OnInit {
 
+  duplicateTitles: any[] = [];
+  duplicateMovieDetails: any;
+  loading: boolean;
+
   constructor(
     private router: Router,
     private apis: ApiService,
-    private sm: SitemapService
-  ) { }
-
-  ngOnInit() {
+    private sm: SitemapService,
+    private fS: FirebaseService
+  ) {
+    this.getAllMoviesResults();
   }
+
+  ngOnInit(): void { }
 
   // Get Data From TMDB API
   getAPIConfig(): void {
@@ -39,6 +46,68 @@ export class SettingsComponent implements OnInit {
 
   generateMovieGenresSitemap(): void {
     this.sm.generateMoviesSitemap();
+  }
+
+
+  getAllMoviesResults(): void {
+    this.duplicateTitles = [];
+    this.loading = true;
+    this.fS.getAllMoviesResults()
+      .then(res => {
+        this.checkDuplicateMovieTitles('slug', res)
+          .then(duplicates => {
+            for (let i = 0; i < duplicates.length; i++) {
+              // console.log(duplicates[i]);
+              const element = duplicates[i];
+              this.getDuplicateMovieDetailsBySlug(element)
+                .then(movie => {
+                  // console.log(movie);
+                  this.duplicateTitles.push(movie);
+                  // console.log(this.duplicateTitles);
+                  this.loading = false;
+                });
+            }
+          });
+      });
+  }
+
+  checkDuplicateMovieTitles(propertyName, collection): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      const duplicateTitles = [];
+      const moviesObject = {};
+      collection.map((item) => {
+        const moviePropertyName = item[propertyName];
+        if (moviePropertyName in moviesObject) {
+          duplicateTitles.push(moviePropertyName);
+        } else {
+          moviesObject[moviePropertyName] = item;
+          delete item.duplicate;
+        }
+      });
+      resolve(duplicateTitles);
+    });
+  }
+
+  getDuplicateMovieDetailsByTitle(movieTitle: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.fS.getMoviesByTitle(movieTitle)
+        .then(res => {
+          resolve(res);
+          // this.duplicateMovieDetails = res;
+          // console.log(this.duplicateMovieDetails);
+        });
+    });
+  }
+
+  getDuplicateMovieDetailsBySlug(movieSlug: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.fS.getMoviesBySlug(movieSlug)
+        .then(res => {
+          resolve(res);
+          // this.duplicateMovieDetails = res;
+          // console.log(this.duplicateMovieDetails);
+        });
+    });
   }
 
 }
