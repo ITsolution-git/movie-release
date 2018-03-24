cbc736962fe3923fc471e1739d33f9323ad4d69a
itsolution-git <roy.smith0820@gmail.com>
2018-03-24 23:02:53 +0200

    sidenav-working-example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,45 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { MediaMatcher } from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+
+  fillerContent = Array(50).fill(0).map(() =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  private _mobileQueryListener: () => void;
+  // shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+
   constructor(
-    public toastr: ToastsManager,
-    vRef: ViewContainerRef
-  ) {
+    changeDetectorRef: ChangeDetectorRef,
+     media: MediaMatcher,
+     public toastr: ToastsManager,
+     vRef: ViewContainerRef
+    ) {
+
     this.toastr.setRootViewContainerRef(vRef);
     document.body.className = 'home';
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
 }
