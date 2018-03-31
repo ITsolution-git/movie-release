da1cb6654211315777268b3c97f89efff711ce5b
itsolution-git <roy.smith0820@gmail.com>
2018-03-31 06:19:43 -0700

    Improved urlOptimizeText(), added removeAcent()

--- src/app/core/services/app.service.ts
+++ src/app/core/services/app.service.ts
@@ -19,9 +19,38 @@ export class AppService {
     private afDb: AngularFireDatabase
   ) { }
 
-  urlOptimizeText(text: string): string {
-    const urlText = text.toLowerCase().replace(/[^a-zA-Z0-9-$@]+/gm, '-');
-    return urlText;
+  urlOptimizeText(text: string): Promise<string> {
+    // console.log('URL Optimize:', text);
+    const noSpecial = text.replace(/\(|\)|\?|\!|\'|\.|\,|\:|\;|\<|\>|\[|\]|\*|\+/g, '');
+    const noDouble = noSpecial.replace(/\œ/g, 'oe').replace(/\ᴂ/g, 'ae');
+    // console.log('no brachets: ', noSpecial);
+    return new Promise<string>((resolves, rejects) => {
+      this.removeAccents(noDouble)
+        .then(res => {
+          // console.log(res);
+          const replaceAnd = res.replace(/\&/g, 'and');
+          const urlText = replaceAnd.toLowerCase().replace(/[^a-zA-Z0-9-$@]+/gm, '-');
+          resolves(urlText);
+        });
+    });
+  }
+
+  removeAccents(p): Promise<string> {
+    // console.log('TEXT TO CONVERT: ', p);
+    const c = 'áàãâäåéèêëíìîïóòõôöøúùûüçćÁÀÃÂÄÅÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇĆ';
+    const s = 'aaaaaaeeeeiiiioooooouuuuccAAAAAAEEEEIIIIOOOOOUUUUCC';
+    let n = '';
+    return new Promise<string>((resolve, reject) => {
+      for (let i = 0; i < p.length; i++) {
+        if (c.search(p.substr(i, 1)) >= 0) {
+          n += s.substr(c.search(p.substr(i, 1)), 1);
+        } else {
+          n += p.substr(i, 1);
+        }
+      }
+      // console.log('CONVERTED TEXT: ', n);
+      resolve(n);
+    });
   }
 
   seoOptimizeText(text: string): string {
@@ -65,6 +94,6 @@ export class AppService {
           console.log(res[0]['url']);
           resolve(res[0]);
         });
-    })
+    });
   }
 }
