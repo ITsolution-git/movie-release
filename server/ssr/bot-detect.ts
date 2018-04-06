e6092b61ab418e273db808d4d4eaef00528b05a1
itsolution-git <roy.smith0820@gmail.com>
2018-04-06 08:38:40 -0700

    Updated Bots List

--- server/ssr/bot-detect.ts
+++ server/ssr/bot-detect.ts
@@ -8,36 +8,42 @@ import { APP_URL, RENDER_URL, CACHE_CONTROL_VALUE } from '../constants';
 // Bot detect
 export const detectBot = (userAgent: string): boolean => {
     // List of bots to target, add more if you'd like
-    // const bots = [
-    //     // crawler bots
-    //     'googlebot',
-    //     'bingbot',
-    //     'yandexbot',
-    //     'duckduckbot',
-    //     'slurp',
-    //     // link bots
-    //     'twitterbot',
-    //     'facebookexternalhit',
-    //     'linkedinbot',
-    //     'embedly',
-    //     'baiduspider',
-    //     'pinterest',
-    //     'slackbot',
-    //     'vkShare',
-    //     'facebot',
-    //     'outbrain',
-    //     'W3C_Validator'
-    // ];
+    const bots = [
+        // crawler bots
+        'googlebot',
+        'bingbot',
+        'yandexbot',
+        'duckduckbot',
+        'slurp',
+        'baiduspider',
+        'ia_archiver',
+        // link bots
+        'twitterbot',
+        'facebookexternalhit',
+        'facebot',
+        'linkedinbot',
+        'embedly',
+        'baiduspider',
+        'pinterest',
+        'slackbot',
+        'vkShare',
+        'facebot',
+        'outbrain',
+        'W3C_Validator',
+        // SEO Tools
+        'screaming frog seo spider',
+        'Pingdom.com_bot_version_1.4_(http://www.pingdom.com/)'
+    ];
 
-    // const agent = userAgent.toLowerCase();
+    const agent = userAgent.toLowerCase();
 
-    // for (const bot of bots) {
-    //     if (agent.indexOf(bot) > -1) {
-    //         console.log('bot detected', bot, agent);
-    //         return true;
-    //     }
-    // }
-    // console.log('no bots found');
+    for (const bot of bots) {
+        if (agent.indexOf(bot) > -1) {
+            console.log('bot detected', bot, agent);
+            return true;
+        }
+    }
+    console.log('no bots found');
     return false;
 };
 
