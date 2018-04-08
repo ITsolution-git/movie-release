d0c4a130c2fb19639bda3a87ad3a2b38ed1f55da
itsolution-git <roy.smith0820@gmail.com>
2018-04-08 05:47:41 -0700

    Created Shared Movie Cards Component and added it to all Components where needed

--- src/app/content/shared/movie-card/movie-card.component.ts
+++ src/app/content/shared/movie-card/movie-card.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AppService } from '../../../core/services/app.service';
+// Constants
+import { TMDB_IMAGES_BASE_URL, IMG_300 } from '../../../constants';
+
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
@@ -7,9 +10,19 @@ import { AppService } from '../../../core/services/app.service';
 })
 export class MovieCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() movie: any[];
 
-  ngOnInit() {
+  TMDB_IMAGES_BASE_URL: any;
+  IMG_300: any;
+
+  constructor(
+    public as: AppService
+  ) {
+    // Initialize Constants
+    this.TMDB_IMAGES_BASE_URL = TMDB_IMAGES_BASE_URL;
+    this.IMG_300 = IMG_300;
   }
+
+  ngOnInit(): void { }
 
 }
