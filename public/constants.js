3414792ba7488d4162a28fe4d611409b06ade877
itsolution-git <roy.smith0820@gmail.com>
2018-04-13 01:09:01 +0300

    seo service, index update

--- public/constants.js
+++ public/constants.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // CORE
-exports.APP_ROOT_URL = 'https://currentmoviereleases.com/';
+exports.APP_ROOT_URL = 'https://currentmoviereleases.com';
 // export const APP_URL = 'https://currentmoviereleases.com'; // PROD
 exports.APP_URL = 'http://localhost:5001'; // DEV
 exports.RENDER_URL = 'https://render-tron.appspot.com/render';
+// export const RENDER_URL = 'https://renderer.currentmoviereleases.com';
 exports.CACHE_CONTROL_VALUE = 'public, max-age=300, s-maxage=600';
 // TMDB
 exports.TMDB_API_URL_ROOT = 'https://api.themoviedb.org/';
--- src/app/core/services/seo/seo.service.ts
+++ src/app/core/services/seo/seo.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 // Constants
 import { APP_SEO_NAME } from '../../../constants';
+import { APP_BASE_URL } from '../../../constants';
+import { DEFAULT_POSTER_IMG } from '../../../constants';
 @Injectable()
 export class SeoService {
 
@@ -20,6 +22,23 @@ export class SeoService {
     this.meta.updateTag(
       { name: 'keywords', content: description + ' | ' + APP_SEO_NAME },
     );
+    this.meta.updateTag(
+      { property: 'og:title',  content: APP_SEO_NAME },
+    );
+    this.meta.updateTag(
+      { property: 'og:url',  content: APP_BASE_URL },
+    );
+    this.meta.updateTag(
+      { property: 'og:type',  content: 'website'},
+    );
+    this.meta.updateTag(
+      { property: 'og:title',  content: APP_SEO_NAME },
+    );
+    this.meta.updateTag(
+      { property: 'og:description',  content: description.substring(0, 300) + ' | ' + APP_SEO_NAME },
+    );
+    this.meta.updateTag(
+      { property: 'og:image',  content: DEFAULT_POSTER_IMG },
+    );
   }
-
 }
